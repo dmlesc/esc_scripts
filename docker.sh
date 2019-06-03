aaaac4511095dddaddaac7d9d3fcfa1ea5ab1189
dmlesc <private@email.io>
2019-06-02 23:23:03 -0600

    docker, node_exporter

---
+++ docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+apt remove docker docker-engine docker.io containerd runc
+apt update
+apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt update
+apt -y install docker-ce docker-ce-cli containerd.io
+docker --version
+
+curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+usermod -aG docker dml
---
+++ node_exporter.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cd /opt
+wget https://github.com/prometheus/node_exporter/releases/download/v0.18.0/node_exporter-0.18.0.linux-amd64.tar.gz
+tar xzvf node_exporter-0.18.0.linux-amd64.tar.gz
+
+cat > /lib/systemd/system/node_exporter.service <<EOF
+[Unit]
+Description=node_exporter
+After=network.target
+
+[Service]
+User=root
+ExecStart=/opt/node_exporter-0.18.0.linux-amd64/node_exporter --no-collector.arp --no-collector.bcache --no-collector.conntrack --no-collector.edac --no-collector.entropy --no-collector.infiniband --no-collector.ipvs --no-collector.loadavg --no-collector.mdadm --no-collector.nfs --no-collector.nfsd --no-collector.netstat --no-collector.sockstat --no-collector.stat --no-collector.textfile --no-collector.time  --no-collector.timex --no-collector.uname --no-collector.vmstat --no-collector.wifi --no-collector.xfs
+WorkingDirectory=/opt/node_exporter-0.18.0.linux-amd64
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable node_exporter
+systemctl daemon-reload
+service node_exporter start
+
+curl localhost:9100/metrics
+rm node_exporter-0.18.0.linux-amd64.tar.gz
