8fd980e7dfbf655736a638f75a5fdc3c2cd6b710
dmlesc <dmlesc>
2020-01-22 22:26:26 -0700

    armv7

---
+++ node_exporter_armv7.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cd /opt
+wget https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-armv7.tar.gz
+tar xzvf node_exporter-0.18.1.linux-armv7.tar.gz
+
+cat > /lib/systemd/system/node_exporter.service <<EOF
+[Unit]
+Description=node_exporter
+After=network.target
+
+[Service]
+User=root
+ExecStart=/opt/node_exporter-0.18.1.linux-armv7/node_exporter --no-collector.arp --no-collector.bcache --no-collector.conntrack --no-collector.edac --no-collector.entropy --no-collector.infiniband --no-collector.ipvs --no-collector.loadavg --no-collector.mdadm --no-collector.nfs --no-collector.nfsd --no-collector.netstat --no-collector.sockstat --no-collector.stat --no-collector.textfile --no-collector.time  --no-collector.timex --no-collector.uname --no-collector.vmstat --no-collector.wifi --no-collector.xfs
+WorkingDirectory=/opt/node_exporter-0.18.1.linux-armv7
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable node_exporter
+systemctl daemon-reload
+service node_exporter start
+
+curl localhost:9100/metrics
+rm node_exporter-0.18.1.linux-armv7.tar.gz
